cfb23141d3eda7b0d50b22a9f742305a851da10d
WeimarC <>
2021-09-22 16:51:18 -0300

    a

--- src/components/SeccionPrincipal.jsx
+++ src/components/SeccionPrincipal.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import Tarjeta from './Tarjeta'
-import "../styles/SeccionPrincipal.module.css"
+import React from "react";
+import { Card } from "react-bootstrap";
+import Tarjeta from "./Tarjeta";
+import "../styles/SeccionPrincipal.module.css";
+
+const infoPersonajes = [
+  {
+    nombre: "Blim Blam",
+    urlImagen: "https://rickandmortyapi.com/api/character/avatar/50.jpeg",
+  },
+  {
+    nombre: "Plumber Rick",
+    urlImagen: "https://rickandmortyapi.com/api/character/avatar/267.jpeg",
+  },
+];
+
+const transformarInfoPersonajes = ({ nombre, urlImagen }) => {
+  return <Tarjeta urlImagen={urlImagen} nombrePersonaje={nombre} />;
+};
+
+const tarjetas = infoPersonajes.map(transformarInfoPersonajes);
 
 const SeccionPrincipal = () => {
-    return (
-        <main>
-           <div>
-               <h1>The Rick and Morty API</h1>
-           </div>
-           <div className="Tarjetero">
-            <Tarjeta/>
-            <Tarjeta/>
-            <Tarjeta/>
-            <Tarjeta/>
-            <Tarjeta/>
-            <Tarjeta/>
-           </div>
-        </main>
-    )
-}
+  return (
+    <main>
+      <div>
+        <h1>The Rick and Morty API</h1>
+      </div>
+      <div className="Tarjetero">
+        <infoPersonajes />
+      </div>
+    </main>
+  );
+};
 
-export default SeccionPrincipal
+export default SeccionPrincipal;
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import "../styles/Tarjeta.module.css"
-const Tarjeta = () => {
-    return (
-        <div className="Tarjeta">
-            <div className="ImagenTarjeta">
-                <img src="Armothy.jpeg" alt="imagen" />
-            </div>
-            <div className="DescripcionTarjeta">
-                <h4><a href="">Nombre Personaje</a></h4>
-                <p className="Estado">
-                    Estado - Tipo de Criatura
-                </p>
-                <div className="LocacionTarjeta">
-                    <h5>Ultima locacion conocida</h5>
-                    <p>Localizacion</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+import React from "react";
+import "../styles/Tarjeta.module.css";
 
-export default Tarjeta
+const Tarjeta = () => {
+  return (
+    <div className="Tarjeta">
+      <div className="ImagenTarjeta">
+        <img src="Armothy.jpeg" alt="imagen" />
+      </div>
+      <div className="DescripcionTarjeta">
+        <h4>
+          <a href="">Nombre Personaje</a>
+        </h4>
+        <p className="Estado">Estado - Tipo de Criatura</p>
+        <div className="LocacionTarjeta">
+          <h5>Ultima locacion conocida</h5>
+          <p>Localizacion</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tarjeta;
